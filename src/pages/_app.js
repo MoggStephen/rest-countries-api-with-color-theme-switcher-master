97f0a9af803f841a2c8520c436e0216bb100020d
MoggStephen <moggstephen66@gmail.com>
2023-05-03 21:45:44 +0100

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+//Import google font!
+import { Nunito_Sans } from 'next/font/google'
+const nunitoSans = Nunito_Sans({ subsets: ['latin'], weight: ['300','600','800'] })
+//Import bootstrap and import useEffect for bootstraps bundled js.
+import 'bootstrap/scss/bootstrap.scss';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  //IMPORT BUNDLED BOOTSTRAP JS
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+    <main className={nunitoSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
---
+++ src/pages/api/get-countries.js
@@ -0,0 +1,38 @@
+// Uses a cached version of the data so that if "https://restcountries.com/v3.1/all" ever breaks we can use the last working result from it.
+let countriesCache = null;
+
+export default async function handler(req, res) {
+
+  // On first load we need to fetch the data
+  // We can tell its the first load if countriesCache is null
+  if (countriesCache === null) {
+    await fetchCountries();
+    res.status(200).json(countriesCache);
+  } 
+  // If not the first load we already have the data cached and can return it
+  else {
+    res.status(200).json(countriesCache);
+  }
+}
+
+// Refresh the data every day
+setInterval(() => {
+  fetchCountries();
+}, 86400000); // 24 hours in milliseconds
+
+async function fetchCountries(){
+  try {
+    // Get the data
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    // Cache the data 
+    countriesCache = data;
+    // Quick time log
+    console.log(`Fetched new data at ${new Date().toLocaleTimeString()}`);
+  } 
+  // If restcountries ever breaks just catch and log it!
+  // We have countriesCache as a backup
+  catch (error) {
+    console.error('restcountries api error: ', error);
+  }
+}
